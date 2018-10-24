ffd81f5493143679d9416dfc76de120f7405328f
edems <adamtorok@sch.bme.hu>
2018-10-24 20:05:42 +0200

    main test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "FrameHandler.h"
+
+int main(int argc, char * argv[]) {
+
+    FrameHandler frameHandler;
+    frameHandler.openCamera(0);
+    frameHandler.readCameraParameters("cam2.calib");
+    frameHandler.init(&argc, argv);
+    frameHandler.run();
+
     return 0;
 }
