d9b92e1d22e0d9a1277e84d330b36859a33888a7
edems <adamtorok@sch.bme.hu>
2018-11-18 20:55:37 +0100

    asset path changed

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char * argv[]) {
 
     FrameHandler frameHandler;
     frameHandler.openCamera(0);
-    frameHandler.readCameraParameters("cam2.calib");
+    frameHandler.readCameraParameters("assets/cam2.calib");
     frameHandler.init(&argc, argv);
     frameHandler.run();
 
