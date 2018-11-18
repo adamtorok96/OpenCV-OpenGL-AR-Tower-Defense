6746d62e8ef00169a61fffc7ce15ea70ff3b5081
edems <adamtorok@sch.bme.hu>
2018-11-18 20:56:40 +0100

    fixes

--- GameObject.cpp
+++ GameObject.cpp
@@ -1,6 +1,6 @@
 #include "GameObject.h"
 
-void GameObject::setPosition(Detector * detector, const Vec3d &rvec, const Vec3d &tvec) {
+void GameObject::setPosition(const Detector * detector, const Vec3d &rvec, const Vec3d &tvec) {
     this->rvec = rvec;
     this->tvec = tvec;
     this->modelViewMatrix = detector->getModelViewMatrix(rvec, tvec);
--- GameObject.h
+++ GameObject.h
@@ -15,7 +15,7 @@ protected:
     Vec3d rvec, tvec;
 
 public:
-    void setPosition(Detector * detector, const Vec3d & rvec, const Vec3d & tvec);
+    void setPosition(const Detector * detector, const Vec3d & rvec, const Vec3d & tvec);
     double getDistance(const GameObject & gameObject);
 
     virtual void process() = 0;
