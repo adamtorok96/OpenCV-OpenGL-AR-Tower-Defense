0def6fb3da81b2b4b2a627128aa4c074b3ec530f
edems <adamtorok@sch.bme.hu>
2018-11-18 16:15:14 +0100

    GameObjectLoader

---
+++ GameObjectLoader.h
@@ -0,0 +1,27 @@
+#ifndef OPENCV_OPENGL_AR_TOWERDEFENSE_GAMEOBJECTLOADER_H
+#define OPENCV_OPENGL_AR_TOWERDEFENSE_GAMEOBJECTLOADER_H
+
+#include <fstream>
+
+#include <GL/gl.h>
+
+#include <nlohmann/json.hpp>
+
+#include "GameObject.h"
+
+using namespace std;
+using json = nlohmann::json;
+
+class GameObjectLoader {
+
+    static GameObjectLoader * instance;
+
+    map<string, GLuint> models;
+
+public:
+    static void load();
+    static GameObjectLoader * getInstance();
+};
+
+
+#endif //OPENCV_OPENGL_AR_TOWERDEFENSE_GAMEOBJECTLOADER_H
